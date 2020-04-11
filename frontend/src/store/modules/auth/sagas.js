4f17e14d46c91af8ba1522ffe9271097ab586700
gabrielmellooliveira <gabrielmello3011@gmail.com>
2020-04-11 13:42:15 -0300

    Create user register

--- frontend/src/store/modules/auth/sagas.js
+++ frontend/src/store/modules/auth/sagas.js
@@ -11,7 +11,8 @@ export function* signIn({ payload }) {
 		const { email, password } = payload;
 
 		const response = yield call(api.post, 'sessions', {
-			email, password
+			email,
+			password,
 		});
 
 		const { token, user } = response.data;
@@ -20,6 +21,8 @@ export function* signIn({ payload }) {
 			toast.error('Usuário não é prestador');
 			return;
 		}
+
+		api.defaults.headers.Authorization = `Bearer ${token}`;
 
 		yield put(signInSuccess(token, user));
 
@@ -30,4 +33,38 @@ export function* signIn({ payload }) {
 	}
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+	try {
+		const { name, email, password } = payload;
+
+		yield call(api.post, 'users', {
+			name,
+			email,
+			password,
+			provider: true,
+		});
+
+		history.push('/');
+	} catch (error) {
+		toast.error('Falha no cadastro, verifique seus dados');
+		yield put(signFailure());
+	}
+}
+
+export function setToken({ payload }) {
+	if (!payload) {
+		return;
+	}
+
+	const { token } = payload.auth;
+
+	if (token) {
+		api.defaults.headers.Authorization = `Bearer ${token}`;
+	}
+}
+
+export default all([
+	takeLatest('persist/REHYDRATE', setToken),
+	takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+	takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
