3f649514e4418ef2347dec20d01dc74a5b864a24
gabrielmellooliveira <gabrielmello3011@gmail.com>
2020-04-11 00:05:38 -0300

    Add authentication

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
+
+import { signInRequest, signInSuccess } from '~/store/modules/auth/actions';
 
 import logo from '~/assets/logo.svg';
 
@@ -13,7 +16,12 @@ const schema = Yup.object().shape({
 });
 
 export default function SignIn() {
-	function handleSubmit(data) {}
+	const dispatch = useDispatch();
+	const loading = useSelector(state => state.auth.loading)
+
+	function handleSubmit({ email, password }) {
+		dispatch(signInSuccess(email, password));
+	}
 
 	return (
 		<>
@@ -27,7 +35,9 @@ export default function SignIn() {
 					placeholder="Sua senha"
 				/>
 
-				<button type="submit">Acessar</button>
+				<button type="submit">
+					{ loading ? 'Carregando...' : 'Acessar' }
+				</button>
 				<Link to="/register">Criar conta gratuita</Link>
 			</Form>
 		</>
