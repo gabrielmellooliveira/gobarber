55bb00cea237985522fb1f895ff867d8e69ea191
gabrielmellooliveira <gabrielmello3011@gmail.com>
2020-04-16 23:06:24 -0300

    Create Background component

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   plugins: ["react", "jsx-a11y", "import", "react-hooks", "prettier"],
   rules: {
-      "prettier/prettier": "error",
+      "prettier/prettier": "warn",
       "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
       "import/prefer-default-export": "off",
       "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
